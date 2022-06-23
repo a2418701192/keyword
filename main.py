342938f8f9165264e8862464af6216f0a3b73bf9
a2418701192 <2418701192@qq.com>
2022-06-23 11:12:49 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def callback():
+    print("start web server process")
+
+if __name__ == '__main__':
+    # 创建命令行解析器
+    parser=argparse.ArgumentParser()
+    #  add_argument() 方法，该方法用于指定程序能够接受哪些命令行选项。
+    parser.add_argument()
+
+
